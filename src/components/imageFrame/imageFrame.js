686f6076782df4ca021296ab0b11f560cf66c381
Nick S <nsardonini@gmail.com>
2021-04-29 15:27:13 -0400

    add media query & hover/active styles

--- src/components/imageFrame/imageFrame.js
+++ src/components/imageFrame/imageFrame.js
@@ -7,6 +7,17 @@ const StyledFrame = styled.div`
   height: 200px;
   transition: opacity 300ms ease-in;
   opacity: ${({ inView }) => (inView ? 100 : 0)};
+  cursor: pointer;
+
+  &:hover,
+  &:active {
+    z-index: 2;
+    filter: brightness(0.7);
+  }
+
+  @media (max-width: 540px) {
+    height: 100px;
+  }
 `;
 
 export default function ImageFrame({ children }) {
