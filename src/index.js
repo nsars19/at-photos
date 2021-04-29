5edd144f5b9388294f9cf39f29feb38976f367cd
Nick S <nsardonini@gmail.com>
2021-04-29 13:16:43 -0400

    add base app styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import colors from "./utils/colors";
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  body {
+    background: ${colors.black}
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
