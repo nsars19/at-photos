d9350368e99d780d12b36030caf64a43c2a18c3b
Nick S <nsardonini@gmail.com>
2021-04-29 14:38:34 -0400

    add styles to ImageFrame

--- src/components/imageFrame/imageFrame.js
+++ src/components/imageFrame/imageFrame.js
@@ -1,11 +1,23 @@
 import styled from "styled-components";
+import colors from "./../../utils/colors";
+import { useInView } from "react-intersection-observer";
 
 const StyledFrame = styled.div`
   overflow: hidden;
-  height: 350px;
-  width: 500px;
+  height: 200px;
+  transition: opacity 300ms ease-in;
+  opacity: ${({ inView }) => (inView ? 100 : 0)};
 `;
 
 export default function ImageFrame({ children }) {
-  return <StyledFrame>{children}</StyledFrame>;
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <StyledFrame ref={ref} inView={inView}>
+      {children}
+    </StyledFrame>
+  );
 }
