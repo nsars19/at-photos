0f92f5bc7e7aba4d4e304cfdec2d9ed41fd7e9b7
Nick S <nsardonini@gmail.com>
2021-04-28 22:26:59 -0400

    add components to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import ProgressiveImage from "./components/progressiveImage/ProgressiveImage";
+
+const StyledApp = styled.div`
+  .img-load {
+    height: 400px;
+  }
+
+  img {
+    height: 100%;
+    transition: filter 300ms ease-in;
+  }
+
+  .image {
+    height: 400px;
+    width: 400px;
+    object-fit: fill;
+  }
+`;
+
 function App() {
-  return <div></div>;
+  const [keys, setKeys] = useState(null);
+
+  useEffect(() => {
+    async function getKeys() {
+      const res = await fetch("http://localhost:3000/photos");
+      const data = await res.json();
+      setKeys(data);
+    }
+
+    getKeys();
+  }, []);
+
+  const mapKeys =
+    keys &&
+    keys.slice(0, 9).map(({ _id, key }) => (
+      <div key={_id}>
+        <ProgressiveImage imgKey={key} />
+      </div>
+    ));
+
+  return <StyledApp>{mapKeys || <div>Loading...</div>}</StyledApp>;
 }
 
 export default App;
