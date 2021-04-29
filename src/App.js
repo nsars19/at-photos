2c1649b2dde49c3c96122019e4ddb5c01e4546e6
Nick S <nsardonini@gmail.com>
2021-04-29 15:26:45 -0400

    add landing section, add media queries for img-wrap container

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import styled from "styled-components";
 import React, { useEffect, useState } from "react";
 import ProgressiveImage from "./components/progressiveImage/ProgressiveImage";
 import ImageFrame from "./components/imageFrame/imageFrame";
+import colors from "./utils/colors";
 
 const StyledApp = styled.div`
-  display: grid;
-  gap: 5px;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-auto-flow: row dense;
-  justify-content: center;
-  width: 99%;
-  margin: 0 auto;
+  .img-wrap {
+    display: grid;
+    gap: 2px;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-auto-flow: row;
+    justify-content: center;
+    width: 98%;
+    margin: 0 auto;
+  }
+
+  section {
+    height: 100vh;
+    display: grid;
+    place-items: center;
+    color: ${colors.blue};
+    h1 {
+      font-size: 2em;
+    }
+  }
+
+  @media (min-width: 1080px) {
+    .img-wrap {
+      width: 80%;
+    }
+  }
+
+  @media (max-width: 540px) {
+    .img-wrap {
+      width: 99%;
+      grid-template-columns: repeat(auto-fill, minmax(90px, 1fr));
+    }
+  }
 `;
 
 function App() {
@@ -36,7 +62,14 @@ function App() {
       </React.Fragment>
     ));
 
-  return <StyledApp>{mapKeys || <div>Loading...</div>}</StyledApp>;
+  return (
+    <StyledApp>
+      <section>
+        <h1>Welcome</h1>
+      </section>
+      <div className="img-wrap">{mapKeys || <div>Loading...</div>}</div>
+    </StyledApp>
+  );
 }
 
 export default App;
