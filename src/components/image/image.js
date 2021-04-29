8cc6db2852541940bdddd59ce2de38d75d3a5c22
Nick S <nsardonini@gmail.com>
2021-04-29 14:38:02 -0400

    move Image component styles into the component's module

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,8 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const StlyedImage = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  transition: filter 300ms ease-in, opacity 300ms ease-in;
+`;
 
 export default function Image({ src, alt, style, className }) {
   return (
-    <img
+    <StlyedImage
       src={src}
       alt={alt || "desc"}
       style={style ? style : {}}
